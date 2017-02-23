58a9c88dbfb1712ab04105fd6280e85ae592d2e0
PHYSXGH <physx_ms@outlook.com>
2017-02-23 19:07:09 +0000

    lkj

---
+++ hashcodeee/src/hashcodeee/VideoSizes.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcodeee;
+
+/**
+ *
+ * @author Robert
+ */
+public class VideoSizes {
+    
+    int size;
+    
+    void videoSize(int size) {
+        size = this.size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+}
