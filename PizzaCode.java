c9530b9db68bfd5792c05bf5bdf96b70353ae51d
Daniel McKinley <SpartanHedgey@gmail.com>
2017-02-23 17:10:52 +0000

    pizza

---
+++ PizzaCode.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzacode;
+
+import java.io.*;
+
+/**
+ *
+ * @author raymu
+ */
+public class PizzaCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world");
+        try{
+    FileInputStream fstream = new FileInputStream("data/medium.in");
+    BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+    
+    String strLine;
+    String toppings;
+    
+    while((strLine = br.readLine()) != null){
+        System.out.println(strLine);
+        //toppings = toppings + strLine;
+    }
+     fstream.close();
+     
+            System.out.println(strLine);
+    }catch (Exception e){//Catch exception if any
+  System.err.println("Error: " + e.getMessage());
+  }
+  }
+    }
+    
+    
+
