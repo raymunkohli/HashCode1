34e55f14c7bbcb4d8d002defb3d63972fc41606c
raymunkohli <raymun1@hotmail.co.uk>
2017-02-23 18:41:00 +0000

    .

---
+++ hashcodeproject/src/hashcodeproject/videoSize.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcodeproject;
+
+/**
+ *
+ * @author raymu
+ */
+public class videoSize {
+    int id,size;
+    videoSize(int id, int size){
+        this.id=id;
+        this.size = size;
+    }
+    
+}
